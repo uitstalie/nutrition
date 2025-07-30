ca1193ed37b09bd29143a70513a27257e65c461b
pc <daiqiancheng0924@126.com>
2025-07-30 21:03:59 +0800

    init

---
+++ src/client/java/com/uitstalie/nutrition/client/NutritionClient.java
@@ -0,0 +1,10 @@
+package com.uitstalie.nutrition.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NutritionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/uitstalie/nutrition/Nutrition.java
@@ -0,0 +1,10 @@
+package com.uitstalie.nutrition;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Nutrition implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
